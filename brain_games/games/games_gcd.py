2e262c8ebb6ef9b8916db9d5080f2405b0f56cc1
GHMan2021 <statio2009@gmail.com>
2021-08-15 10:41:57 +0400

    fix brain-gcd

--- brain_games/games/games_gcd.py
+++ brain_games/games/games_gcd.py
@@ -6,11 +6,14 @@ def is_gcd():
     print('Find the greatest common divisor of given numbers.')
     counter = 1
     while counter <= 3:
-        number1 = randint(0, 25)
+        number1 = randint(0, 0)
         number2 = randint(0, 25)
         print('Question:', number1, number2)
         user_answer = str(input('Your answer: '))
-        right_answer = str(gcd(number1, number2))
+        if abs(number1 + number2) == max(abs(number1), abs(number2)):
+            right_answer = str(max(abs(number1), abs(number2)))
+        else:
+            right_answer = str(gcd(number1, number2))
 
         if f_check_answer(right_answer, user_answer):
             counter += 1
